30a6ec6ea8a405df9153d5f097a6f0fd7621e9dc
Lewis <lewis.warner@tomorrows.co.uk>
2020-12-18 11:22:14 +0000

    Add project files.

---
+++ JessAssignment/LoginForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace JessAssignment
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(734, 473);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(456, 38);
+            this.usernameTB.TabIndex = 0;
+            this.usernameTB.TextChanged += new System.EventHandler(this.usernameTB_TextChanged);
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(728, 413);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(145, 32);
+            this.UsernameLabel.TabIndex = 1;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1978, 1218);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.usernameTB);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.Label UsernameLabel;
+    }
+}
+
